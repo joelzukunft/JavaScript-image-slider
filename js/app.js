ae1858e5d5132286a297dba49c22f209f1cd2085
Tomczak <Marta.Tomczak@capco.com>
2020-01-14 14:04:01 +0100

    JS new logic

--- js/app.js
+++ js/app.js
@@ -1,16 +1,15 @@
 const slider = document.querySelector(".slider");
 
-// Przycisk "prev" do przesuwania zdjęć wstecz i przycisk "next"
+// "Previous" and "Next" button
 const prev = slider.querySelector("#prevPicture");
 const next = slider.querySelector("#nextPicture");
 
 const mySlides = slider.querySelectorAll("li");
 
-// indeks obrazka, który jest aktualnie widoczny
+// index of the image currently displayed on screen
 let indexOfImg = 0;
 mySlides[indexOfImg].className = "visible";
 
-// ---------------------
 
 let list = [];
 mySlides.forEach(function(el) {
@@ -20,31 +19,41 @@ console.log(list);
 
 list[0].className = "visible";
 
-// ----------------------
 
+// Adding "visible" class
+function makeItVisible(a) {
+    list[a].className = "visible";
+}
 
-// eventy reagujące na kliknięcie
+// Removing "visible" class
+function makeItInvisible(b) {
+    list[b].className = "";
+}
+
+// "Click" events:
+// Previous image
 prev.addEventListener("click", function () {
     indexOfImg--;
     if (indexOfImg < 0) {
         indexOfImg = list.length-1
-        list[0].className = "";
-        list[indexOfImg].className = "visible";
+        makeItInvisible(0);
+        makeItVisible(indexOfImg);
     } else {
-        list[indexOfImg+1].className = "";
-        list[indexOfImg].className = "visible";
+        makeItInvisible(indexOfImg+1);
+        makeItVisible(indexOfImg);
     }
 })
 
+// Next image
 next.addEventListener("click", function () {
     indexOfImg++;
     if (indexOfImg >= list.length) {
         indexOfImg = 0;
-        list[list.length-1].className = "";
-        list[indexOfImg].className = "visible";
+        makeItInvisible(list.length-1);
+        makeItVisible(indexOfImg);
     } else {
-        list[indexOfImg-1].className = "";
-        list[indexOfImg].className = "visible";
+        makeItInvisible(indexOfImg-1);
+        makeItVisible(indexOfImg);
     }
 })
 
